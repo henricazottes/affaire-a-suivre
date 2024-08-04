963a239c7145a2623be93db6cdb9e800e59c0453
Henri Cazottes <henri.cazottes@lifen.fr>
2024-08-04 19:02:57 +0200

    feat: Add bootstrap and basic layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def active_link_to(text = nil, path = nil, active_classes: '', **options, &)
+    path ||= text
+
+    classes = active_classes.presence || 'active'
+    options[:class] = class_names(options[:class], 'nav-link')
+    options[:class] = class_names(options[:class], classes) if current_page?(path)
+
+    return link_to(path, options, &) if block_given?
+
+    link_to text, path, options
+  end
 end
